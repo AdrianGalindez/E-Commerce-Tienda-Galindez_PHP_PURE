99a8bca76e6c0558f87a2a51912bccc556d66520
AdrianGalindez <adriangalindez@unicomfacauca.edu.co>
2026-04-18 23:36:25 -0500

    Add files via upload

--- config/Database.php
+++ config/Database.php
@@ -7,35 +7,24 @@ class Database {
     private $user = "root";
     private $pass = "";
     private $port = "3306";
-
     public $conn;
 
     public function connect(){
-
         try{
-
             $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->db};charset=utf8mb4";
-
             $this->conn = new PDO(
                 $dsn,
                 $this->user,
                 $this->pass
             );
-
             $this->conn->setAttribute(
                 PDO::ATTR_ERRMODE,
                 PDO::ERRMODE_EXCEPTION
             );
-
         }
         catch(PDOException $e){
-
             die("Error de conexión: " . $e->getMessage());
-
         }
-
         return $this->conn;
-
     }
-
 }
